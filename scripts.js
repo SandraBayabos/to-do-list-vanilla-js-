decbe582e803c0cd03d7413f032f53e3c8664ea6
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-10-02 21:51:26 +0800

    todolist files for frontend sept 2019 intake

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// GLOBAL VARIABLES //
+let addItemButton = document.getElementById("addButton");
+let todoList = document.getElementById("unordered-list");
+
+// ADD ITEM TO LIST //
+function addToList() {
+  let enteredItem = document.getElementById("list-item").value;
+  console.log(enteredItem);
+  if (enteredItem === "") {
+    alert("Wow. Nothing to do. Must be nice.");
+  }
+
+  if (enteredItem.length > 3) {
+    addItemButton.disabled = false;
+    const listElement = document.createElement("LI");
+    let enteredElement = document.createTextNode(enteredItem);
+    console.log(enteredElement);
+    listElement.appendChild(enteredElement);
+    todoList.appendChild(listElement);
+    document.getElementById("list-item").value = "";
+    listElement.setAttribute("id", "list");
+
+    // DELETE ITEM FROM LIST //
+    // FOR LOOP TO GET EACH LIST ITEM //
+    let listItems = document.getElementsByTagName("LI");
+    for (
+      let i = 0;
+      i < listItems.length;
+      i++ // ONCLICK OF EACH ITEM REMOVECHILD //
+    ) {
+      listItems[i].onclick = function() {
+        listItems[i].parentElement.removeChild(listItems[i]);
+        // this.parentNode.removeChild(this);
+        // listItems[i].style.display = "none";
+      };
+    }
+  } else {
+    alert("That's a little short. Expand.");
+  }
+}
